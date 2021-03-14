86ae80dd4739cc6014a8f67f37fdf77013b35389
artemzaharov <58831797+artemzaharov@users.noreply.github.com>
2021-03-14 14:08:29 +0100

    Delete production.py

--- alternativa_tv/settings/production.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-
-DEBUG = False
-SECRET_KEY = 'ily59&dfggsdfgdsfgfsdsdfd0=06*=86(^f*kynugu*jl_vlokh@%r9sjddzzf0r=b8'
-ALLOWED_HOSTS = ['alternativatv.ru','www.alternativatv.ru']
-try:
-    from .local import *
-except ImportError:
-    pass
