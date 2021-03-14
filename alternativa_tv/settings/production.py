edfc0fbf309f6c59e6c4c5c1ce45cb8f24ca481d
Your Name <you@example.com>
2021-03-14 12:28:45 +0000

    try server pull

--- alternativa_tv/settings/production.py
+++ alternativa_tv/settings/production.py
@@ -1,7 +1,8 @@
 from .base import *
 
 DEBUG = False
-
+SECRET_KEY = 'ily59&d0=06*=86(^f*kynugu*jl_vlokh@%r9sjddzzf0r=b8'
+ALLOWED_HOSTS = ['alternativatv.ru','www.alternativatv.ru']
 try:
     from .local import *
 except ImportError:
